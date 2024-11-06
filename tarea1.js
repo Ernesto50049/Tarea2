02ba39a22ad9e5dae4ccfed6e10f09a51c5644d3
Ernesto50049 <angelernestoabril@gmail.com>
2024-11-06 15:10:06 -0700

    Add files via upload

---
+++ tarea1.js
@@ -0,0 +1,6 @@
+let nombre="Angel Ernesto Abril Mazatán";
+
+function contarLetras(nombre) {
+    return nombre.length;
+ }
+    console.log("El número de letras en el nombre es:", contarLetras(nombre));
